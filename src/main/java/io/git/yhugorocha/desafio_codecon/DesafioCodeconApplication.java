b4fe8887805ae893514fd1f8dcf79fb0f21e61ec
Hugo Silva <jose.silva@extreme.digital>
2025-04-23 15:04:31 -0300

    feat: first commit

---
+++ src/main/java/io/git/yhugorocha/desafio_codecon/DesafioCodeconApplication.java
@@ -0,0 +1,13 @@
+package io.git.yhugorocha.desafio_codecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCodeconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCodeconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/git/yhugorocha/desafio_codecon/DesafioCodeconApplicationTests.java
@@ -0,0 +1,13 @@
+package io.git.yhugorocha.desafio_codecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCodeconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
