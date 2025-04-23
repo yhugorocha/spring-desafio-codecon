35a448cd47b2145aeedeba1d06ba3c4e9d5de6a0
Hugo Silva <jose.silva@extreme.digital>
2025-04-23 15:05:32 -0300

    feat: Adding Controller

---
+++ src/main/java/io/git/yhugorocha/desafio_codecon/controller/UserController.java
@@ -0,0 +1,45 @@
+package io.git.yhugorocha.desafio_codecon.controller;
+
+import io.git.yhugorocha.desafio_codecon.dto.ImportResponse;
+import io.git.yhugorocha.desafio_codecon.dto.SuperUserResponse;
+import io.git.yhugorocha.desafio_codecon.dto.TopCountriesResponse;
+import io.git.yhugorocha.desafio_codecon.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@RequestMapping(value="/users")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ImportResponse> importUsers(@RequestParam("file") MultipartFile file) throws IOException {
+
+        return ResponseEntity.ok(userService.importUsers(file));
+    }
+
+    @GetMapping("/superusers")
+    public ResponseEntity<SuperUserResponse> getSuperUsers(){
+        long start  = System.currentTimeMillis();
+        var superUsers = userService.getSuperUsers();
+        long elapsed = System.currentTimeMillis() - start;
+        return ResponseEntity.ok(new SuperUserResponse(LocalDateTime.now(), elapsed, superUsers));
+    }
+
+    @GetMapping("/top-countries")
+    public ResponseEntity<TopCountriesResponse> getTopCountries(){
+        long start  = System.currentTimeMillis();
+        var top = userService.getTopCountries();
+        long elapsed = System.currentTimeMillis() - start;
+        return ResponseEntity.ok(new TopCountriesResponse(LocalDateTime.now(), elapsed, top));
+    }
+}
